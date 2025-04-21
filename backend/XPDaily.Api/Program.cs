78e68d5d7d8000e6614ab083d946208efb4cdda4
Gabriel Rosendo <gabrielrosendoleitesilva@gmail.com>
2025-04-21 08:29:11 -0300

    Adding CORS.

--- backend/XPDaily.Api/Program.cs
+++ backend/XPDaily.Api/Program.cs
@@ -4,6 +4,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// CORS (Para permitir chamadas do frontend)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") // porta padrão do Vite
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -33,6 +45,8 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+// TEST ROUTE
+// Esta rota é apenas para teste, para verificar se o backend está funcionando corretamente
 app.MapGet("/api/hello", () => Results.Json(new { message = "Olá do backend!" }))
    .WithName("Hello");
 
